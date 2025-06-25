0b23065f4b5990848896c8795f6d7ad4f1915606
HilmiCode <102717128+HilmiRidho@users.noreply.github.com>
2025-06-25 10:17:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
 firebase.initializeApp({
   apiKey: "AIzaSyDf64ttRcxVtyv_xhb06bHopD1kUiFJI9Y",
   authDomain: "hilmicode-comment.firebaseapp.com",
